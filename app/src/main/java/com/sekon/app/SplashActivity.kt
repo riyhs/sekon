dbb81f9bce11c764814466f5fb27910475381f14
Riyaldi Hasan <riy.2923@gmail.com>
2020-10-28 11:43:00 +0700

    add Icon Launcher and logo for SplashActivity.kt

--- app/src/main/java/com/sekon/app/SplashActivity.kt
+++ app/src/main/java/com/sekon/app/SplashActivity.kt
@@ -1,12 +1,18 @@
 package com.sekon.app
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val startColor = window.statusBarColor
+        val endColor = ContextCompat.getColor(this, R.color.colorAccent)
+        ObjectAnimator.ofArgb(window, "statusBarColor", startColor, endColor).start()
 
         supportActionBar?.hide()
     }
